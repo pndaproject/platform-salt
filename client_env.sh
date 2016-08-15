aaf7713b89bbc00861172648a3cee52c6840403b
James Clarke <jamclar2@cisco.com>
2016-08-15 11:05:01 +0100

    Use separate API key for s3
    Using a separate API key for S3 prevents the API key with full access,
    as used to create PNDA, from being stored on instances for use at
    runtime.

--- client_env.sh
+++ client_env.sh
@@ -1,5 +1,9 @@
+# API key for full API access for use creating PNDA
 export AWS_ACCESS_KEY_ID=xxxx
 export AWS_SECRET_ACCESS_KEY=xxxx
+# API key for s3 access only for PNDA to use at runtime
+export S3_ACCESS_KEY_ID=xxxx
+export S3_SECRET_ACCESS_KEY=xxxx
 export AWS_REGION=eu-west-1
 # Standard 64-bit Ubuntu 14.04
 export AWS_IMAGE_ID=ami-f95ef58a
