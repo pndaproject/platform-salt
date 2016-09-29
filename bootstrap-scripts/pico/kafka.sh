f8f8fc0218ecc1993b44d5cb51d56416f3b3d05d
James Clarke <jamclar2@cisco.com>
2016-09-29 14:49:14 +0100

    Include node index in name for kafka brokers on pico

--- bootstrap-scripts/pico/kafka.sh
+++ bootstrap-scripts/pico/kafka.sh
@@ -11,14 +11,14 @@ roles:
   - kafka
 
 pnda_cluster: $PNDA_CLUSTER
-broker_id: 0
+broker_id: $1
 EOF
 
 cat >> /etc/salt/minion <<EOF
-id: $PNDA_CLUSTER-kafka
+id: $PNDA_CLUSTER-kafka-$1
 EOF
 
-echo $PNDA_CLUSTER-kafka > /etc/hostname
-hostname $PNDA_CLUSTER-kafka
+echo $PNDA_CLUSTER-kafka-$1 > /etc/hostname
+hostname $PNDA_CLUSTER-kafka-$1
 
 service salt-minion restart
