8285dff4fa8768c1102ce3d2d8d685940ebd0337
oruchlis <oruchlis@cisco.com>
2016-12-05 16:24:56 +0200

    Adding Logstash cluster support

--- cli/pnda-cli.py
+++ cli/pnda-cli.py
@@ -89,7 +89,7 @@ def display_elasped():
     CONSOLE.info("%sTotal execution time: %s%s", blue, str(elapsed), reset)
 
 
-def generate_template_file(flavor, datanodes, opentsdbs, kafkas, zookeepers, esmasters, esingests, esdatas, escoords, esmultis):
+def generate_template_file(flavor, datanodes, opentsdbs, kafkas, zookeepers, esmasters, esingests, esdatas, escoords, esmultis, logstashNodes):
     common_filepath = 'cloud-formation/cf-common.json'
     with open(common_filepath, 'r') as template_file:
         template_data = json.loads(template_file.read())
@@ -122,6 +122,8 @@ def generate_template_file(flavor, datanodes, opentsdbs, kafkas, zookeepers, esm
         instance_escoordinator = json.dumps(template_data['Resources'].pop('instanceESCoordinator'))
     if 'instanceESMulti' in template_data['Resources']:
         instance_esmulti = json.dumps(template_data['Resources'].pop('instanceESMulti'))
+    if 'instanceLogstash' in template_data['Resources']:
+        instance_logstash = json.dumps(template_data['Resources'].pop('instanceLogstash'))
 
     for datanode in range(0, datanodes):
         instance_cdh_dn_n = instance_cdh_dn.replace('$node_idx$', str(datanode))
@@ -158,6 +160,10 @@ def generate_template_file(flavor, datanodes, opentsdbs, kafkas, zookeepers, esm
     for esmulti in range(0, esmultis):
         instance_esmulti_n = instance_esmulti.replace('$node_idx$', str(esmulti))
         template_data['Resources']['instanceESMulti%s' % esmulti] = json.loads(instance_esmulti_n)
+
+    for eslogstash in range(0, esmultis):
+        instance_logstash_n = instance_logstash.replace('$node_idx$', str(eslogstash))
+        template_data['Resources']['instanceESMulti%s' % esmulti] = json.loads(instance_logstash_n)
 
     return json.dumps(template_data)
 
@@ -625,6 +631,7 @@ def main():
     esIngestNodes = PNDA_ENV['elk-cluster']['INGEST_NODES']
     esCoordinatorNodes = PNDA_ENV['elk-cluster']['COORDINATING_NODES']
     esMultiNodes = PNDA_ENV['elk-cluster']['MULTI_ROLE_NODES']
+    logstashNodes = PNDA_ENV['elk-cluster']['LOGSTASH_NODES']
 
     # Branch defaults to master
     # but may be overridden by pnda_env.yaml
@@ -776,6 +783,8 @@ def main():
         esCoordinatorNodes = 0
     if esMultiNodes is None:
         esMultiNodes = 0
+    if logstashNodes is None:
+        logstashNodes = 0
 
     node_limit("datanodes", datanodes)
     node_limit("opentsdb-nodes", tsdbnodes)
@@ -786,9 +795,12 @@ def main():
     node_limit("elk-es-ingest", esIngestNodes)
     node_limit("elk-es-coordinator", esCoordinatorNodes)
     node_limit("elk-es-multi", esMultiNodes)
+    node_limit("elk-logstash", logstashNodes)
 
     template_data = generate_template_file(flavor, datanodes, tsdbnodes, kafkanodes, zknodes,
-                                           esMasterNodes, esIngestNodes, esDataNodes, esCoordinatorNodes, esMultiNodes)
+                                           esMasterNodes, esIngestNodes, esDataNodes, esCoordinatorNodes,
+                                           esMultiNodes, logstashNodes)
+
     console_dns = create(template_data, pnda_cluster, flavor, keyname, no_config_check, branch)
     CONSOLE.info('Use the PNDA console to get started: http://%s', console_dns)
     CONSOLE.info(' Access hints:')
