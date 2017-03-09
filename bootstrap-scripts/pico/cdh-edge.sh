59fd2ac1c4a041021f7b4eaa7ac44d80c8ace77c
James Clarke <jamclar2@cisco.com>
2017-03-09 11:32:28 +0000

    Refactor saltmaster bootstrap code
    Remove duplication between saltmaster bootstrap scripts for different
    flavors.
    
    PNDA-2802

--- bootstrap-scripts/pico/cdh-edge.sh
+++ bootstrap-scripts/pico/cdh-edge.sh
@@ -9,136 +9,8 @@
 # variables
 set -ex
 
-DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
-
-# Install a saltmaster and minion, plus saltmaster config
-if [ "x$DISTRO" == "xubuntu" ]; then
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get -y install unzip git salt-master
-fi
-
-if [ "x$DISTRO" == "xrhel" ]; then
-yum -y install unzip git salt-master
-fi
-
-cat << EOF > /etc/salt/master
-## specific PNDA saltmaster config
-auto_accept: True      # auto accept minion key on new minion provisioning
-
-fileserver_backend:
-  - roots
-  - minion
-
-file_roots:
-  base:
-    - /srv/salt/platform-salt/salt
-
-pillar_roots:
-  base:
-    - /srv/salt/platform-salt/pillar
-
-# Do not merge top.sls files across multiple environments
-top_file_merging_strategy: same
-
-# To autoload new created modules, states add and remove salt keys,
-# update bastion /etc/hosts file automatically ... add the following reactor configuration
-reactor:
-  - 'minion_start':
-    - salt://reactor/sync_all.sls
-  - 'salt/cloud/*/created':
-    - salt://reactor/create_bastion_host_entry.sls
-  - 'salt/cloud/*/destroying':
-    - salt://reactor/delete_bastion_host_entry.sls
-## end of specific PNDA saltmaster config
-file_recv: True
-
-failhard: True
-EOF
-
-# Set up platform-salt that contains the scripts the saltmaster runs to install software
-mkdir -p /srv/salt
-cd /srv/salt
-
-if [ "x$PLATFORM_GIT_REPO_URI" != "x" ]; then
-  git clone -q --branch $PLATFORM_GIT_BRANCH $PLATFORM_GIT_REPO_URI
-elif [ "x$PLATFORM_URI" != "x" ] ; then
-  mkdir -p /srv/salt/platform-salt && cd /srv/salt/platform-salt && \
-  wget -q -O - $PLATFORM_URI | tar -zvxf - --strip=1 && ls -al && \
-  cd -
-elif [ "x$PLATFORM_SALT_LOCAL" != "x" ]; then
-  tar zxf /tmp/$PLATFORM_SALT_TARBALL -C /srv/salt
-else
-  exit 2
-fi
-
-# Push pillar config into platform-salt for environment specific config
-cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
-os_user: $OS_USER
-keystone.user: ''
-keystone.password: ''
-keystone.tenant: ''
-keystone.auth_url: ''
-keystone.region_name: ''
-aws.apps_region: '$PNDA_APPS_REGION'
-aws.apps_key: '$PNDA_APPS_ACCESS_KEY_ID'
-aws.apps_secret: '$PNDA_APPS_SECRET_ACCESS_KEY'
-pnda.apps_container: '$PNDA_APPS_CONTAINER'
-pnda.apps_folder: '$PNDA_APPS_FOLDER'
-aws.archive_region: '$PNDA_APPS_REGION'
-aws.archive_key: '$PNDA_APPS_ACCESS_KEY_ID'
-aws.archive_secret: '$PNDA_APPS_SECRET_ACCESS_KEY'
-pnda.archive_container: '$PNDA_ARCHIVE_CONTAINER'
-pnda.archive_type: 's3a'
-pnda.archive_service: ''
-EOF
-
-cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
-pnda_mirror:
-  base_url: '$PNDA_MIRROR'
-  misc_packages_path: /mirror_misc/
-
-cloudera:
-  parcel_repo: '$PNDA_MIRROR/mirror_cloudera'
-
-anaconda:
-  parcel_version: "4.0.0"
-  parcel_repo: '$PNDA_MIRROR/mirror_anaconda'
-
-pip:
-  index_url: '$PNDA_MIRROR/mirror_python/simple'
-
-packages_server:
-  base_uri: '$PNDA_MIRROR'
-EOF
-
-if [ "$PR_FS_TYPE" == "swift" ] ; then
-cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
-package_repository:
-  fs_type: 'swift'
-EOF
-elif [ "$PR_FS_TYPE" == "s3" ] ; then
-cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
-package_repository:
-  fs_type: 's3'
-EOF
-elif [ "$PR_FS_TYPE" == "sshfs" ] ; then
-cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
-package_repository:
-  fs_type: "sshfs"
-  fs_location_path: "$PR_FS_LOCATION_PATH"
-  sshfs_user: "$PR_SSHFS_USER"
-  sshfs_host: "$PR_SSHFS_HOST"
-  sshfs_path: "$PR_SSHFS_PATH"
-  sshfs_key: "$PR_SSHFS_KEY"
-EOF
-else
-cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
-package_repository:
-  fs_type: "$PR_FS_TYPE"
-  fs_location_path: "$PR_FS_LOCATION_PATH"
-EOF
-fi
+echo $PNDA_CLUSTER-cdh-edge > /etc/hostname
+hostname $PNDA_CLUSTER-cdh-edge
 
 service salt-master restart
 
@@ -181,8 +53,5 @@ EOF
 cat >> /etc/salt/minion <<EOF
 id: $PNDA_CLUSTER-cdh-edge
 EOF
-
-echo $PNDA_CLUSTER-cdh-edge > /etc/hostname
-hostname $PNDA_CLUSTER-cdh-edge
 
 service salt-minion restart
