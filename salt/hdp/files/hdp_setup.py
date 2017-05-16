2ee097ea292c12950882fd0b0b18d67b2a7f45c1
James Clarke <jamclar2@cisco.com>
2017-05-16 10:24:25 +0100

    Configure Ambari with pnda mirror URLs for HDP stack repos
    PNDA-2949

--- salt/hdp/files/hdp_setup.py
+++ salt/hdp/files/hdp_setup.py
@@ -9,6 +9,7 @@ Created:    15/05/2017
 """
 
 import logging
+import requests
 
 DEFAULT_LOG_FILE = '/var/log/pnda/hadoop_setup.log'
 
@@ -27,7 +28,7 @@ def setup_hadoop(
         anaconda_repo=None,
         anaconda_version=None):
 
-    logging.info("Doesn't do anything yet, here are the parameters:")
+    logging.info("setup_hadoop:")
     logging.info(ambari_api)
     logging.info(nodes)
     logging.info(cluster_name)
@@ -38,4 +39,23 @@ def setup_hadoop(
     logging.info(anaconda_repo)
     logging.info(anaconda_version)
 
+    logging.info("Configuring Ambari to use HDP stack repos")
 
+    if 'ubuntu14' in hdp_core_stack_repo:
+        hdp_os_type = 'ubuntu14'
+    elif 'centos7' in hdp_core_stack_repo:
+        hdp_os_type = 'centos7'
+    else:
+        raise Exception('Expected ubuntu14 or centos7 in hdp_core_stack_repo but found: %s' % hdp_core_stack_repo)
+
+    repo_requests = [('http://%s:8080/api/v1/stacks/HDP/versions/2.6/operating_systems/%s/repositories/HDP-2.6' % (ambari_api, hdp_os_type),
+                      '{"Repositories" : { "base_url" : "%s", "verify_base_url" : true }}' % hdp_core_stack_repo),
+                      ('http://%s:8080/api/v1/stacks/HDP/versions/2.6/operating_systems/%s/repositories/HDP-UTILS-1.1.0.21' % (ambari_api, hdp_os_type),
+                      '{"Repositories" : { "base_url" : "%s", "verify_base_url" : true }}' % hdp_utils_stack_repo)]
+
+    headers = {'X-Requested-By': ambari_username}
+    for repo_request in repo_requests:
+        response = requests.put(repo_request[0], repo_request[1], 
+                                auth=(ambari_username, ambari_password), headers=headers)
+        if response.status_code != 200:
+            raise Exception(response.text)
