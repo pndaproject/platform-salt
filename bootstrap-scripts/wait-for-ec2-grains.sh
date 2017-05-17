7d72d7af829076051758977727cd08049e2b89e6
Julien Barbot <jubarbot@cisco.com>
2017-05-17 11:31:21 +0200

    Remove wait-for-ec2-grains.sh as we don't use ec2 grains anymore

--- bootstrap-scripts/wait-for-ec2-grains.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -v
-echo 'Checking ec2 grains are present on all minions...'
-GRAIN_RETRIES=0
-until [[ $(sudo salt "*" grains.get ec2 | grep region | wc -l) -eq $(sudo salt "*" grains.get pnda | grep flavor | wc -l) ]] || [[ $GRAIN_RETRIES -eq "180" ]]; do
-  sleep 1
-  echo waiting for ec2 grains, retry $(( GRAIN_RETRIES++ )) of 180
-done
